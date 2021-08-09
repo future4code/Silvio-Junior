6a7a45d8146ca9c0a262ab943d7beff473892dfb
Silvio Ribeiro Dias Jr <silviordjr@outlook.com>
2021-08-09 15:04:04 -0300

    Desafio do dia

---
+++ semana1/desafio/index.js
@@ -0,0 +1,9 @@
+let primeiroNumero = Number(prompt("Informe o primeiro número:"))
+let segundoNumero = Number(prompt("Informe o segundo número:"))
+
+let soma = primeiroNumero + segundoNumero
+let multiplicacao = primeiroNumero * segundoNumero
+
+console.log("O primeiro número somado ao segundo resulta em:",soma)
+console.log("O primeiro número multiplicado pelo segundo resulta em:",multiplicacao)
+
