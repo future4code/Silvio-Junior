ff8483455e62b2e83a79c503fd83029ed2991d4a
Silvio Ribeiro Dias Jr <silviordjr@outlook.com>
2021-08-11 15:25:04 -0300

    Desafios do dia

---
+++ semana1/desafioAula05/index.js
@@ -0,0 +1,10 @@
+// Desafios
+//1.
+const frase = prompt("Digite uma frase:")
+const lista = frase.split(" ")
+console.log(lista)
+
+//2.
+const frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+const indice = frutas.indexOf("Abacaxi")
+console.log(`O indice da palavra Abacaxi é: ${indice}\nO tamanho da array é: ${frutas.length}`)
