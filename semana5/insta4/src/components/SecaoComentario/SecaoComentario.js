7a2866bfedae46830d82636d1962cef1f123554e
Silvio Ribeiro Dias Jr <silviordjr@outlook.com>
2021-09-10 17:46:45 -0300

    Exercícios do dia - Insta4 Listas

--- semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -12,6 +12,7 @@ const InputComentario = styled.input`
     margin-right: 5px;
 `
 
+
 export class SecaoComentario extends Component {
 	state = {
 		comentarioUsuario: '',
@@ -24,14 +25,16 @@ export class SecaoComentario extends Component {
 	}
 
 	render() {
-		return <CommentContainer>
-			<InputComentario
-				name={"comentario"}
-				placeholder={'Comentário'}
-				value={this.state.comentarioUsuario}
-				onChange={this.onChangeComentario}
-			/>
-			<button onClick={this.props.aoEnviar}>Enviar</button>
-		</CommentContainer>
+		return (
+			<CommentContainer>
+				<InputComentario
+					name={"comentario"}
+					placeholder={'Comentário'}
+					value={this.state.comentarioUsuario}
+					onChange={this.onChangeComentario}
+				/>
+				<button onClick={this.props.aoEnviar}>Enviar</button>
+			</CommentContainer>
+		)
 	}
 }
