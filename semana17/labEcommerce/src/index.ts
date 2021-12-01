b745ff8ad1de192e827ea922935760d1990b0035
Silvio Ribeiro Dias Jr <silviordjr@outlook.com>
2021-12-01 18:11:35 -0300

    Endpoints

--- semana17/labEcommerce/src/index.ts
+++ semana17/labEcommerce/src/index.ts
@@ -2,6 +2,11 @@ import express, {Express} from 'express'
 import cors from 'cors'
 import getAllUsers from './endpoints/getAllUsers';
 import createUser from './endpoints/createUser';
+import registerProduct from './endpoints/registerProduct';
+import getAllProducts from './endpoints/getAllProducts';
+import registerPurchase from './endpoints/registerPurchase';
+import getAllPurchases from './endpoints/getAllPurchases';
+import getPurchasesFromUser from './endpoints/getPurchasesFromUser';
 
 const app: Express = express();
 
@@ -10,9 +15,20 @@ app.use(cors());
 
 app.get('/users', getAllUsers)
 app.post('/users', createUser)
+app.get('/users/:userId/purchases', getPurchasesFromUser)
+
+app.post('/products', registerProduct)
+app.get('/products', getAllProducts)
+
+app.post('/purchases', registerPurchase)
+app.get('/purchases', getAllPurchases)
 
 
 import { AddressInfo } from "net";
+
+
+
+
 
 
 const server = app.listen(process.env.PORT || 3003, () => {
